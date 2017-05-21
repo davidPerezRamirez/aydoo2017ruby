5792bf8b4872587de9c6724b88bcc4c1033dd858
davidperezramirez <pucarauntref@gmail.com>
2017-05-21 17:29:48 -0300

    test post con parametro alfanumerico devuelve codigo respuesta 400

--- factores-primos-4/spec/app_spec.rb
+++ factores-primos-4/spec/app_spec.rb
@@ -8,22 +8,28 @@ describe 'APP' do
 	    expect(last_response.status).to eq 200
 	end
 
-	it "llamada a post/primos sin parametros deber ser ok" do
+	it "llamada a post/primos sin parametros deberia tener status 200" do
 	    post "/primos"
 
 	    expect(last_response.status).to eq 200
 	end
 
-	it "llamada a post/primos con parametros deber ser ok" do
-	    post "/primos", params = {"x" => 12}
+	it "llamada a post/primos con parametro  12 deberia ser 2,2,3" do
+	    post "/primos", params = {"x" => "12"}
 	    
 	    expect(last_response.body).to eq "2,2,3"
 	end
 
-	it "llamada a get/primos con parametros deber ser ok" do
-	    get "/primos", params = {"x" => 12}
+	it "llamada a get/primos con parametro 12 deberia 3,2,2" do
+	    get "/primos", params = {"x" => "12"}
 	    
 	    expect(last_response.body).to eq "3,2,2"
+	end
+
+	it "llamada a post/primos con parametro alfanumerico deberia tener status 400" do
+	    post "/primos", params = {"x" => "12a"}
+	    
+	    expect(last_response.status).to eq 400
 	end
 
 end
