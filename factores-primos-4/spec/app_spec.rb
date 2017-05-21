f105e6da8e7c5a5bba9bc23cafa3d5b0ae84ed1d
davidperezramirez <pucarauntref@gmail.com>
2017-05-21 17:04:51 -0300

    test metodo post sin parametros

--- factores-primos-4/spec/app_spec.rb
+++ factores-primos-4/spec/app_spec.rb
@@ -2,10 +2,16 @@ require_relative 'spec_helper.rb'
 
 describe 'APP' do
 
-	it "llamada a get/primos deberia ser ok" do
+	it "llamada a get/primos sin paramtros deberia ser ok" do
 	    get "/primos"
+
 	    expect(last_response.status).to eq 200
 	end
 
+	it "llamada a post/primos sin parametros deber ser ok" do
+	    post "/primos"
+
+	    expect(last_response.status).to eq 200
+	end
 
 end
